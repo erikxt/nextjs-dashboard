f288f69f1e3b7a53554ce273291e4f2129dbe3e9
erik <iqboy118@qq.com>
2024-02-25 23:23:13 +0800

    finish chapter 8

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-{/* <div className={styles.shape} />; */ }
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
